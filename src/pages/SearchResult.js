9bdb9e5207155bd401ea873e641e7c8e708744a5
nrj6670 <76197461+nrj6670@users.noreply.github.com>
2020-12-30 11:17:57 +0530

    5. search page loading fix

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -14,35 +14,39 @@ import MovieDetails from "../components/SearchedMovieDetail";
 
 const SearchResult = () => {
   //extracting search results
-  const { searchOutput } = useSelector((state) => state.movieDetail);
+  const { searchOutput, isLoading } = useSelector((state) => state.movieDetail);
 
   //fetching location to load movie detail
   const location = useLocation();
   const pathID = location.pathname.split("/")[3];
 
   return (
-    <SearchList>
-      {pathID && <MovieDetails />}
-      <h2 className="header">Search result : </h2>
-      <Container>
-        {searchOutput.length ? (
-          searchOutput.map((data) => (
-            <Card
-              title={data.name ? data.name : data.title}
-              release={
-                data.release_date ? data.release_date : data.first_air_date
-              }
-              image={data.poster_path}
-              type={data.title ? "movie" : "tv_show"}
-              id={data.id}
-              key={data.id}
-            />
-          ))
-        ) : (
-          <h1>No results found</h1>
-        )}
-      </Container>
-    </SearchList>
+    <>
+      {!isLoading && (
+        <SearchList>
+          {pathID && <MovieDetails />}
+          <h2 className="header">Search result : </h2>
+          <Container>
+            {searchOutput.length ? (
+              searchOutput.map((data) => (
+                <Card
+                  title={data.name ? data.name : data.title}
+                  release={
+                    data.release_date ? data.release_date : data.first_air_date
+                  }
+                  image={data.poster_path}
+                  type={data.title ? "movie" : "tv_show"}
+                  id={data.id}
+                  key={data.id}
+                />
+              ))
+            ) : (
+              <h1>No results found</h1>
+            )}
+          </Container>
+        </SearchList>
+      )}
+    </>
   );
 };
 
